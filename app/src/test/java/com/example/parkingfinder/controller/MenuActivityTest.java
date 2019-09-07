ed1cb9fd2a9b23db01e6c27ec2b81d4f2be01fb1
Timothy Novice <timothyn@iinet.net.au>
2019-09-07 14:58:08 +1000

    Added Unit tests

---
+++ app/src/test/java/com/example/parkingfinder/controller/MenuActivityTest.java
@@ -0,0 +1,12 @@
+package com.example.parkingfinder.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MenuActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+}
---
+++ app/src/test/java/com/example/parkingfinder/view/RegistrationActivityTest.java
@@ -0,0 +1,12 @@
+package com.example.parkingfinder.view;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RegistrationActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+}
