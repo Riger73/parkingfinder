f7737f638136ec1956577eb06cbee1b20f42c892
Timothy Novice <timothyn@iinet.net.au>
2019-09-07 14:29:17 +1000

    Full featured Android front end Added: menu (empty) Registration form Login

--- app/src/main/java/com/example/parkingfinder/view/RegistrationActivity.java
+++ app/src/main/java/com/example/parkingfinder/view/RegistrationActivity.java
@@ -1,33 +1,31 @@
 package com.example.parkingfinder.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.example.parkingfinder.R;
-import com.example.parkingfinder.model.Constants;
-import com.example.parkingfinder.model.data.IPF_Endpoint;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.annotation.NonNull;
-
-import android.text.TextUtils;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.parkingfinder.R;
+import com.example.parkingfinder.model.Constants;
+import com.example.parkingfinder.model.data.DatabaseHelper;
+import com.example.parkingfinder.model.data.IPF_Endpoint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-import static com.example.parkingfinder.R.id.bRegistration;
-
 public class RegistrationActivity extends AppCompatActivity {
-    private TextView mTextMessage;
-    private EditText firstName;
-    private EditText lastName;
+    DatabaseHelper db;
+    EditText mTextUsername;
+    EditText mTextPassword;
+    EditText mTextCnfPassword;
+    Button mButtonRegister;
+    TextView mTextViewLogin;
     private IPF_Endpoint mPF_Endpoint;
 
     // Initialises retrofit HTTP transfer - gets data from endpoint
@@ -38,57 +36,57 @@ public class RegistrationActivity extends AppCompatActivity {
                 .build();
         mPF_Endpoint = retrofit.create(IPF_Endpoint.class);
     }
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_activity);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        firstName = findViewById(R.id.firstName);
-        lastName = findViewById(R.id.lastName);
-
-        Button bRegistration = findViewById(R.id.bRegistration);
-
-        bRegistration.setOnClickListener(new View.OnClickListener() {
+        db = new DatabaseHelper(this);
+        mTextUsername = (EditText)findViewById(R.id.edittext_username);
+        mTextPassword = (EditText)findViewById(R.id.edittext_password);
+        mTextCnfPassword = (EditText)findViewById(R.id.edittext_cnf_password);
+        mButtonRegister = (Button)findViewById(R.id.button_register);
+        mTextViewLogin = (TextView)findViewById(R.id.textview_login);
+        mTextViewLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                checkDataEntered();
+                Intent LoginIntent =
+                        new Intent(RegistrationActivity.this,LoginActivity.class);
+                startActivity(LoginIntent);
+            }
+        });
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = mTextUsername.getText().toString().trim();
+                String pwd = mTextPassword.getText().toString().trim();
+                String cnf_pwd = mTextCnfPassword.getText().toString().trim();
+
+                if(pwd.equals(cnf_pwd)){
+                    long val = db.addUser(user,pwd);
+                    if(val > 0){
+                        Toast.makeText(
+                                RegistrationActivity.this,
+                                "You have registered",Toast.LENGTH_SHORT).show();
+                        Intent moveToLogin =
+                                new Intent(
+                                        RegistrationActivity.this,LoginActivity.class);
+                        startActivity(moveToLogin);
+                    }
+                    else{
+                        Toast.makeText(
+                                RegistrationActivity.this,
+                                "Registeration Error",Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                else{
+                    Toast.makeText(
+                            RegistrationActivity.this,
+                            "Password is not matching",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
-
-    boolean isEmpty(EditText text) {
-        CharSequence str = text.getText().toString();
-        return TextUtils.isEmpty(str);
-    }
-
-    void checkDataEntered() {
-        if (isEmpty(firstName)) {
-            Toast t = Toast.makeText(this, "This field can't be blank, please enter " +
-                    "your First Name", Toast.LENGTH_SHORT);
-            t.show();
-        }
-        if (isEmpty(lastName)) {
-            lastName.setError("This field can't be blank, please enter your Last Name");
-        }
-    }
 }
+
