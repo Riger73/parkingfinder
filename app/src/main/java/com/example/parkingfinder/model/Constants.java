6be1fcd63a23fee96f6daa07595d333c78449e8a
Timothy Novice <timothyn@iinet.net.au>
2019-09-15 23:14:18 +1000

    Corrected typo

--- app/src/main/java/com/example/parkingfinder/model/Constants.java
+++ app/src/main/java/com/example/parkingfinder/model/Constants.java
@@ -3,5 +3,5 @@ package com.example.parkingfinder.model;
 public class Constants {
     public static final String URL_PF_ENDPOINT =
             "https://avid-invention-249406.appspot.com/";
-
+            //"https://postman-echo.com/";
 }
